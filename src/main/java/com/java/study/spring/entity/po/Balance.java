255fdb0275342e6f00085a6c6f20e672bd2cf68f
caozibiao <1306741518@qq.com>
2020-11-19 19:11:31 +0800

    string

---
+++ src/main/java/com/java/study/spring/entity/po/Balance.java
@@ -0,0 +1,66 @@
+package com.java.study.spring.entity.po;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author caozibiao
+ * @since 2020-11-19
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class Balance extends Model<Balance> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private Integer balance;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+}
---
+++ src/main/java/com/java/study/spring/mapper/BalanceMapper.java
@@ -0,0 +1,16 @@
+package com.java.study.spring.mapper;
+
+import com.java.study.spring.entity.po.Balance;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author caozibiao
+ * @since 2020-11-19
+ */
+public interface BalanceMapper extends BaseMapper<Balance> {
+
+}
