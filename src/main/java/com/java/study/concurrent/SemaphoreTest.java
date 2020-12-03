970756da598915b0d022cec13592d070c68486ea
caozibiao <1306741518@qq.com>
2020-12-03 22:58:47 +0800

    Thread

---
+++ src/main/java/com/java/study/concurrent/SemaphoreTest.java
@@ -0,0 +1,11 @@
+package com.java.study.concurrent;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreTest {
+    Semaphore semaphore = new Semaphore(1);
+    public static void main(String[] args) {
+
+    }
+
+}
