cb068cf829754e1ed6b57c77cd4d20fbc8727cb6
caozibiao <caozibiaosy@chinamobile.com>
2020-12-03 15:21:49 +0800

    oom

--- src/main/java/com/java/study/jvm/error/HeapOOM.java
+++ src/main/java/com/java/study/jvm/error/HeapOOM.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
  * -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails
  */
 public class HeapOOM {
+
     static class OOMObject{}
 
     public static void main(String[] args) {
